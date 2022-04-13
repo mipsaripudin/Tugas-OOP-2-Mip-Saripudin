dd269762e6a9c8a229d873dc323e05cedcd46191
Mip Saripudin <93251067+mipsaripudin@users.noreply.github.com>
2022-04-13 21:54:11 +0700

    update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root', '', 'oopkokab') or die(mysqli_error($con));
+$con = mysqli_connect('localhost', 'root', '', 'wilayah_mipsaripudin') or die(mysqli_error($con));
 if (!$con) {
     echo "Koneksi Gagal!";
 }
